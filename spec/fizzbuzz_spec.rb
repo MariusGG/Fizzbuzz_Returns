26961e901f67b7e093bf4b4d04558d783a0c3459
Marius Guei <marius.g.guei@gmail.com>
2019-02-02 07:35:31 +0000

    failing test for fizz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,5 +9,10 @@ describe 'FizzBuzz' do
   it "when given 1, it returns 1" do
     expect(fizzbuzz(1)).to eq 1
   end
+  context 'Fizzbuzz' do
+    it "returns fizz when passsed 3" do
+      expect(fizzbuzz(3)).to eq "Fizz"
+    end
 
+  end
 end
