d97b79d710915ac1b1ff692f6919a73c692b196a
Marius Guei <marius.g.guei@gmail.com>
2019-02-02 08:28:07 +0000

    refactored fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,8 @@
 
 def fizzbuzz(num)
   raise "Not an iteger" if num.is_a? String
-  if num % 15 == 0
-    return "FizzBuzz"
-  elsif num % 3 == 0
-    return "Fizz"
-  elsif num % 5 == 0
-    return "Buzz"
-  else
-    return num
-  end
+  return "FizzBuzz" if num % 15 == 0
+  return "Fizz" if num % 3 == 0
+  return "Buzz" if num % 5 == 0
+  return num
 end
