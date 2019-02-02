ece18673dff298eed2cf6303173c603a8bf12532
Marius Guei <marius.g.guei@gmail.com>
2019-02-02 07:47:40 +0000

    failing test for 15 and refactored test naming

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,12 +7,16 @@ describe 'FizzBuzz' do
     expect(fizzbuzz(1)).to eq 1
   end
   context 'FizzBuzz' do
-    it "returns fizz when passsed 3" do
+    it "returns fizz when number is divisible by 3" do
       expect(fizzbuzz(3)).to eq "Fizz"
     end
 
-    it "returns buzz when passed 5" do
+    it "returns buzz when number is divisible by 5" do
       expect(fizzbuzz(5)).to eq "Buzz"
+    end
+
+    it "returns FizzBuzz when number is divisible by 15" do
+      expect(fizzbuzz(15)).to eq "FizzBuzz"
     end
 
   end
