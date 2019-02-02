dbda1afac654d65476a4f2bb73d49c8bc0cf488c
Marius Guei <marius.g.guei@gmail.com>
2019-02-02 07:43:00 +0000

    passing test for Buzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,9 +6,13 @@ describe 'FizzBuzz' do
   it "when given 1, it returns 1" do
     expect(fizzbuzz(1)).to eq 1
   end
-  context 'Fizzbuzz' do
+  context 'FizzBuzz' do
     it "returns fizz when passsed 3" do
       expect(fizzbuzz(3)).to eq "Fizz"
+    end
+
+    it "returns buzz when passed 5" do
+      expect(fizzbuzz(5)).to eq "Buzz"
     end
 
   end
